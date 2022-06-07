17bd95b48d513826d9642d5f927bebff4c1b42c0
Sonal Varshney <sonalvrshny@gmail.com>
2022-06-07 15:39:59 -0400

    database connection

---
+++ shortener_app/database.py
@@ -0,0 +1,19 @@
+# code for database connection 
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .config import get_settings
+
+# engine is like the entry point to the database
+# declarative_base returns a class that connects the database engine to 
+# the SQLAlchemy functionality of the models 
+engine = create_engine(
+    get_settings().db_url, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
+Base = declarative_base()
+
