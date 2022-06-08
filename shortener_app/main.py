ed8ebff905d79bbf0acbc4e0017c480c32b52786
Sonal Varshney <sonalvrshny@gmail.com>
2022-06-08 12:55:54 -0400

    connected to database

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,14 +1,26 @@
 # FastAPI implementation that has one endpoint 
 
-from curses.ascii import HT
+import secrets
 import validators
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
-from . import schemas
+from . import models, schemas
+from .database import SessionLocal, engine
 
 # define app by instantiating FastAPI
 # the app variable is the main point of interaction to create the API
 app = FastAPI()
+
+# binds the database engine
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
@@ -23,11 +35,23 @@ def read_root():
     return "Welcome to URL Shortener app :)"
 
 # create_url endpoint expects a URL string as POST request 
-@app.post("/url")
-def create_url(url: schemas.URLBase):
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request("Your provided URL is not valid")
-    return f"TODO:Create database entry for: {url.target_url}"
+
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
+    return db_url
 
 # to run the app, we need a server 
 # $ uvicorn shortener_app.main:app --reload
