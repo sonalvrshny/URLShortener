5d08d0080a8698899b742a930f48688b9cf2710d
Sonal Varshney <sonalvrshny@gmail.com>
2022-06-10 09:46:00 -0400

    manage urls using admin key

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,12 +1,14 @@
 # FastAPI implementation that has one endpoint 
 
 import validators
+from starlette.datastructures import URL
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from . import models, schemas, keygen, crud
 from .database import SessionLocal, engine
+from .config import get_settings
 
 # define app by instantiating FastAPI
 # the app variable is the main point of interaction to create the API
@@ -48,10 +50,8 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     # key = "".join(secrets.choice(chars) for _ in range(5))
     # secret_key = "".join(secrets.choice(chars) for _ in range(8))
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
 
-    return db_url
+    return get_admin_info(db_url)
 
 # can GET requests from URL provided as argument
 @app.get("/{url_key}")
@@ -62,6 +62,24 @@ def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(
     else:
         raise_not_found(request)
 
+# GET URL info using admin secret key
+@app.get("/admin/{secret_key}", name="administration info", response_model=schemas.URLInfo,)
+def get_url_info(
+    secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
+    else:
+        raise_not_found(request)
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
 
 # to run the app, we need a server 
 # $ uvicorn shortener_app.main:app --reload
